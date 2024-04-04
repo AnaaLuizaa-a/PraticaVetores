1faec0e1ecf46f53c83be42d5fa12b2cccfcea6f
Ana Luiza <164022945+AnaaLuizaa-a@users.noreply.github.com>
2024-04-04 11:10:51 -0300

    Add files via upload

---
+++ questao 12.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+      // imprimir todos numeros pares de 1 a 50
+      for(int i = 1; i<=50;i++){
+        if(i%2==0){
+          System.out.println("Contagem dos produtos: " +i);
+        }
+        
+      }
+    
+      }
+
+    }
